6deb3e2b60497a9fc5fcef5ae6e182f55ed0dec8
Claudio <claudio@tracking.exposed>
2019-10-03 10:11:19 +0200

    feedback on personal page

--- static/js/personal.js
+++ static/js/personal.js
@@ -11,18 +11,16 @@ function personal() {
     const url = buildApiUrl('personal', pk); // `/personal/${pk}/`);
 
     $.getJSON(url, (data) => {
+        console.log(data);
         console.log("personal API, retrieved recent videos:", _.size(data.recent));
+
+        /* usare il valore "total" per comporre o modificare la paginazione */
 
         let lastTimed = "";
         _.each(data.recent, function(video) {
-            if(lastTimed != video.relative) {
-                addTimeHeader(video.relative);
-                lastTimed = video.relative;
-            }
-            //console.log(video);
-            buildTable(video);
+            addVideoRow(video);
         });
-        updateProfileInfo(data.profile);
+        updateProfileInfo(data.supporter);
     });
 }
 
@@ -30,7 +28,10 @@ function updateProfileInfo(profile) {
     /* da qui devono essere estratti i gruppi ed eventualmente altri metadati,
      per dire all'utente:
      1) ti riconsociamo
-     2) ecco il tuo profilo */
+     2) ecco il tuo profilo
+     AGGIUNGEREI -- 3 ) cancella il tuo tag (Se c'è), aggiungi un nuovo tag
+     così si mescola un riquadro informativo alle uniche 2 attività consentite
+     */
     console.log(profile);
 };
 
@@ -41,56 +42,74 @@ function downloadCSV() {
     window.open(csvurl);
 }
 
-function buildTable(video) {
+function addVideoRow(video) {
     var id = `${video.id}`;
     let tbody = "<tbody id="+id+" style='display: none;'>";
 
-    _.each(video.related, function(r) {
-        console.log(r);
-        tbody += `
-                <tr>
-                    <td scope="col" colspan="2">
-                        <p class="mb-0">
-                            <b class="mr-2">${r.index}</b>
-                            ${r.title}<br />
-                         </p>
-                    </td>
-                    <td>
-                        <small><a class="compare icon-small" href="/compare/#${r.videoId}">compare</a></small>
-                   </td>
-                </tr>
-        `;
-    });
+    // IL MIO USO di alt="" dentro agli <a> è scorretto. vorrei che quell'informazione
+    // uscisse questa non dovrebbe essere una tabella, ma delle <div class="row"
+
     const h = `
-    <table class="table">
-        <thead class="thead-light">
-            <tr>
-                <th class="col-6 align-middle" scope="col">
-                    <h5 class="mb-0">${video.title}</h5>
-                    <p class="mb-0">
-                        <small>by: <a class="compareLink" href="/author/#${video.videoId}">${video.authorName}</a></small> |
-                        <small class="mr-3">${video.viewInfo.viewStr}</small>
-                        <small><a class="compare icon-small" href="/compare/#${video.videoId}">compare this</a></small>
-                    </p>
-                </th>
-                <th class="col-3 align-middle" scope="col">
-                    <p class="mb-0">
-                       <span class="toggle-related btn btn-sm icon-small arrow-down" onclick="toggleRelated('${video.id}', $(this));"> <b>${video.relatedN}</b> Related</span>
-                    </p>
-                </th>
-                <th class="col-3 align-middle" scope="col">
-                    <!--todo: need a working url for deleting-->
-                    <small><a class="delete btn btn-sm icon-small delete" href="api/delete/#${video.videoId}">delete</a></small>
-                </th>
-        </thead>
-        ${tbody}
-    </table>
+    <div class="row">
+      <div class="col-2 align-middle" scope="col">
+        <small> ${video.relative} </small>
+      </div>
+      <div class="col-5 align-middle" scope="col">
+        <small>
+          <a class="icon-small" href="/compare/#${video.videoId}">${video.title}</a>
+        </small>
+      </div>
+      <div class="col-1 align-middle" scope="col">
+        <small class="mb-0">
+          <a
+            class="compare icon-small"
+            alt="Compare all the evidences on ${video.title}"
+            href="/author/#${video.videoId}">
+              [ICONA]
+          </a>
+        </small>
+      </div>
+      <div class="col-1 align-middle" scope="col">
+        <small class="mb-0">
+          <a
+            class="compareLink"
+            alt="Compare all the evidences from ${video.authorName}"
+            href="/author/#${video.videoId}">
+              [ICONA]
+            </a>
+        </small>
+      </div>
+      <div class="col-1 align-middle" scope="col">
+        <small class="mb-0">
+          <a
+            class="compareLink"
+            alt="Compare all the videos suggesting ${video.title}"
+            href="/related/#${video.videoId}">
+              [ICONA]
+          </a>
+        </small>
+      </div>
+      <div class="col-1 align-middle" scope="col">
+          <small>
+            <a
+              class="delete btn btn-sm icon-small delete"
+              onclick="removeEvidence('${video.videoId}')">
+            </a>
+          </small>
+      </div>
     `;
 
     $("#report").append(h);
 };
 
+/*
+    <p class="mb-0">
+       <span class="toggle-related btn btn-sm icon-small arrow-down" onclick="toggleRelated('${video.id}', $(this));"> <b>${video.relatedN}</b> Related</span>
+    </p>
+*/
+
 function toggleRelated(id, $this) {
+  // non capisco a cosa serve
    $('#'+id).toggle('fast','linear', function(){
        if($this.hasClass('arrow-up')) $this.removeClass('arrow-up');
        else  $this.addClass('arrow-up');
@@ -98,6 +117,10 @@ function toggleRelated(id, $this) {
 }
 
 function addTimeHeader(timestring) {
-    const h =`<h4 class="timeheader light-font mb-3 mt-5">${timestring}</h4>`;
+    const h =`<h5 class="timeheader light-font mb-3 mt-5">${timestring}</h5>`;
     $("#report").append(h);
+}
+
+function removeEvidence(id) {
+  console.log("todo removeEvidence", id);
 }
