ac744eb2b0d9092828ac754c5cf673e5b8c9e0ba
Claudio <claudio@tracking.exposed>
2020-09-26 19:07:48 +0200

    implmented first experiment search

---
+++ static/js/sunnyhack.js
@@ -0,0 +1,68 @@
+
+async function queryServerAboutQuerystrings() {
+    const url = buildApiUrl('searches', 'peaceful transition of power', 2);
+    const retval = await $.getJSON(url, function (results) {
+        return results;
+    });
+    return retval;
+}
+
+async function getRecentSearches() {
+    const url = buildApiUrl('search', 'keywords', 2);
+    const retval = await $.getJSON(url, function (results) {
+        return results;
+    });
+    /*  { "selist": [
+     *    {
+     *      "id": "ab12cd34",
+     *      "t": "Qanon",
+     *      "amount": 60,
+     *      "searchId": [
+     *        "38708bd5cc61134ea0763f1b079fc94bcff1a615", "21dc51be94b4b64ea6380f96e031d48818112e31", "7def339253ac062721a2f58cadf90aa739d23945"
+     *      ],
+     *      "searches": 3
+     *    }, {...}, {...}
+     *  ],
+     *  "parameters": {
+     *    "hardcodedAmount": 17,
+     *    "hardcodedUnit": "days",
+     *    "amount": 200,
+     *    "skip": 0
+     *  }}
+     */
+
+    if(!retval || !retval.parameters) 
+        return { error: true, message: "failure in communicating with server"};
+
+    if(!retval.selist || _.size(retval.selist) == 0)
+        return _.merge(retval.parameters, {
+            error: true, message: "we didn't get any search result from the server"});
+
+    if(retval.parameters.overflow)
+        console.log("Manage paging, overflow observed in the last",
+            retval.parameters.hardcodedAmount, retval.parameters.hardcodedUnit);
+
+    console.log("returning", retval.parameters.amount, "over a maximum of", retval.parameters.max);
+    return retval.selist;
+}
+
+function appendLinkList(data, copyFrom, dest) {
+    /* data is a collection from the API above,
+     * copyFrom is CSS selector about a piece of <HTML hidden> acting as a template 
+     * dest is a CSS selector where we append the dynamic content */
+    console.log("appendLinkList", data, copyFrom, dest, "applying alphabetical sorting");
+    _.each(_.orderBy(data, 't'), function(entry) {
+        const div = $(copyFrom).clone();
+        div.removeAttr('hidden');
+        div.attr("id", entry.id);
+        div.appendTo(dest);
+        const idname = "#" + entry.id;
+
+        $(idname + " > .sunnylink > .searchtimes").text(entry.searches);
+        $(idname + " > .sunnylink > .totalvideos").text(entry.amount);
+        $(idname + " > .sunnylink > .linkwrapper > .linktoyoutube").attr('href', 'https://www.youtube.com/results?search_query=' + entry.t);
+        $(idname + " > .sunnylink > .linkwrapper > .linktoyoutube").text(entry.t);
+    });
+
+
+}
